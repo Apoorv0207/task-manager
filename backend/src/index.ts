c67348b902939bfed529185158dff26e02e08807
Apoorv Gautam <apoorvgtm@gmail.com>
2026-04-02 18:23:48 +0530

    Added frontend url for cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,13 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://task-manager-chi-dun.vercel.app'
+  ],
+  credentials: true
+}))
 app.use(express.json())
 
 // Routes
